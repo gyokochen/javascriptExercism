3173dba3545d675c485e12fe3fc12491bf7e9bf0
gyokochen <Chitano.jiang@gmail.com>
2021-07-12 08:08:08 +0200

    Add explaination

--- 0301_leap/leap.js
+++ 0301_leap/leap.js
@@ -1,12 +1,13 @@
 //
 // Expressions & operators practice 
-//
+// This is a simple math practice, the important part is to understand set.
 
 export const isLeap = year => (year % 4 == 0) && (!((year % 100 ==0) && ( year % 400 != 0 ))) ;
 
 
 //https://www.w3schools.com/js/js_comparisons.asp
 //条件运算
-// Another solution in community
+// Another solution in community -- use of Conditional (ternary) operator
+
 //  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
