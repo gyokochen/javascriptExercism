7dd7d6d2021b30a5f705dc45ba16cd5f4023d0bd
gyokochen <Chitano.jiang@gmail.com>
2021-10-09 09:58:51 +0200

    How to reverse string in Javascript

---
+++ 0302_reverse-string/reverse-string.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = string => string.split('').reverse().join('');
+
+//https://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
