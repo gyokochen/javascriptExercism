4e9a3b0d7823cdecea3a25473e5ce1241db6864c
gyokochen <Chitano.jiang@gmail.com>
2021-10-13 09:02:30 +0200

    Pracitce related with array

---
+++ 01_Easy/resistor-color/resistor-color.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = colorValue => {
+    return COLORS.indexOf(colorValue);
+  };
+
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
